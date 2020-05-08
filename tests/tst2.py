4be07900536a440c31e2c06d0b2bda9a968e07de
AlexBirtch <qBNXlrhVmimU9FDLXsT5>
2020-05-08 22:58:00 +1000

    tests

---
+++ tests/tst2.py
@@ -0,0 +1,15 @@
+import unittest
+import ya_app
+
+
+
+class TestYandexTranslator(unittest.TestCase):
+    def test_translate(self):
+        self.assertEqual(ya_app.translate_it('hi')['text'][0], 'привет')
+
+    def test_request_code(self):
+        self.assertEqual(ya_app.translate_it('hi')['code'], 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
